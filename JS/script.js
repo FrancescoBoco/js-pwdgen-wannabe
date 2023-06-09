491b89b83cd84127fa68e8c2133ebc852459ee67
FrancescoBoco <francesco.bocoux@gmail.com>
2023-06-09 15:40:11 +0200

    Esercizio

--- JS/script.js
+++ JS/script.js
@@ -8,3 +8,25 @@ const submitBestColor = prompt ('Inserisci il colore preferito');
 
 
 document.getElementById('my-p').innerHTML = submitName + ' ' + submitAge + ' ' + submitBestColor + ' ' +  101  ;
+
+document.writeln('Primo metodo per scrivere Hello World')
+
+alert('Secondo metodo per scrivere Hello World')
+
+document.getElementById('my-p').innerHTML = ('Terzo metodo per scrivere Hellow Wrold') 
+
+// commento 
+
+// Tipologie di Variabili 
+
+// var 
+// const
+// let
+
+// utilizzare le variabili 
+
+let annoAttuale =  2023
+const dataDiNascitaDiPippo = 2000 
+const etàDiPippo = annoAttuale - dataDiNascitaDiPippo
+
+document.writeln(  etàDiPippo )
