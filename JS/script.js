85137222bdd8c5bec3324a4e0894c84e853757a7
FrancescoBoco <francesco.bocoux@gmail.com>
2023-06-09 15:19:17 +0200

    Fine esercizio

---
+++ JS/script.js
@@ -0,0 +1,10 @@
+const submitName = prompt ('Inserisci il tuo nome');
+const submitAge = prompt ('Inserisci la tua età ');
+const submitBestColor = prompt ('Inserisci il colore preferito');
+
+
+
+
+
+
+document.getElementById('my-p').innerHTML = submitName + ' ' + submitAge + ' ' + submitBestColor + ' ' +  101  ;
