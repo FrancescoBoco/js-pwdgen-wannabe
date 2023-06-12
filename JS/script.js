584c93cae29b3c28c53fe9a70b528d39ba6a7ea0
FrancescoBoco <francesco.bocoux@gmail.com>
2023-06-12 16:14:24 +0200

    Correzione esercizio

--- JS/script.js
+++ JS/script.js
@@ -1,5 +1,5 @@
 const submitName = prompt ('Inserisci il tuo nome');
-const submitAge = prompt ('Inserisci la tua età ');
+const submitSurname = prompt ('Inserisci il tuo cognome');
 const submitBestColor = prompt ('Inserisci il colore preferito');
 
 
@@ -7,26 +7,6 @@ const submitBestColor = prompt ('Inserisci il colore preferito');
 
 
 
-document.getElementById('my-p').innerHTML = submitName + ' ' + submitAge + ' ' + submitBestColor + ' ' +  101  ;
+document.getElementById('my-p').innerHTML = submitName + ' ' + submitSurname + ' ' + submitBestColor + ' ' +  101  ;
 
-document.writeln('Primo metodo per scrivere Hello World')
 
-alert('Secondo metodo per scrivere Hello World')
-
-document.getElementById('my-p').innerHTML = ('Terzo metodo per scrivere Hellow Wrold') 
-
-// commento 
-
-// Tipologie di Variabili 
-
-// var 
-// const
-// let
-
-// utilizzare le variabili 
-
-let annoAttuale =  2023
-const dataDiNascitaDiPippo = 2000 
-const etàDiPippo = annoAttuale - dataDiNascitaDiPippo
-
-document.writeln(  etàDiPippo )
